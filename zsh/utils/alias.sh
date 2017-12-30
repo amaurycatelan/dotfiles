5b1823a1e70156689776b7f9e71c82ab8de6c1f9
Amaury <amaury.catelan@gmail.com>
2017-12-30 12:22:28 -0200

    Fix shell alias and minor updates

--- zsh/utils/alias.sh
+++ zsh/utils/alias.sh
@@ -3,11 +3,11 @@
 # ALIAS (Shell) ,,
 # --------------------------
 
-# Personal settings
+# personal settings
 alias gc="python ${gdrive}/root/projects/lab/git-helper/gclone.py"
 alias wn="sh ${gdrive}/root/projects/lab/typewriter/numbers.sh"
 
-# Directory navigation
+# directory navigation
 alias cl="clear"
 alias ..="cd .."
 alias ...="cd ../.."
@@ -15,59 +15,59 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias ~="cd ~"
 
-# Always recursive and verbose
-alias cp "cp -rv"
-alias rm "rm -rv"
-alias mv "mv -v"
+# always recursive and verbose
+alias cp="cp -rv"
+alias rm="rm -rv"
+alias mv="mv -v"
 
-# List all files colorized in long format
+# list all files colorized in long format
 alias l="ls -l -G"
 alias ls-a="ls -a -G"
-# List all files colorized in long format, including dot files
+# list all files colorized in long format, including dot files
 alias la="ls -la -G"
-# List only directories
+# list only directories
 alias lsd='ls -l -G | grep "^d"'
-# Always use color output for `ls`
-alias ls ="command ls -G"
+# always use color output for `ls`
+alias ls="command ls -G"
 
-# Process status
+# process status
 alias psa="ps aux"
 alias psag="ps aux | ag "
 
-# Easy folder and file loading
+# easy folder and file loading
 alias apps="cd /Applications"
 alias finder="open . -a finder.app"
 
-# Clean up .DS_Store files
+# clean up .DS_Store files
 alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
 
-# File size
+# file size
 alias fs="stat -f \"%z bytes\""
 
-# Show/hide hidden files in Finder
+# show/hide hidden files in Finder
 alias showfiles="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
 alias hidefiles="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
 
-# Hide/show desktop icons
+# hide/show desktop icons
 alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
 alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
 
-# Spotlight
+# spotlight
 alias spotoff="sudo mdutil -a -i off"
 alias spoton="sudo mdutil -a -i on"
 
-# Empty the Trash
+# empty the Trash
 alias emptytrash "rm -rfv ~/.Trash"
 
-# Network
+# network
 alias my-ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ and print $1'"
 
-# View HTTP traffic
+# view HTTP traffic
 alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
 alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
-# Lock the screen (when going AFK)
+# lock the screen (when going AFK)
 alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
 
 # --------------------------
