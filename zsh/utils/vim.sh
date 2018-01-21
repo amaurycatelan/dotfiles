94fb14c875ce854bcfb7de8d35d7d51b5c39aaa4
Amaury <amaury.catelan@gmail.com>
2018-01-21 01:35:52 -0200

    Include vim alias

---
+++ zsh/utils/vim.sh
@@ -0,0 +1,14 @@
+# --------------------------
+
+# VIM ,,
+# --------------------------
+
+# fix vi error: Terminal too wide
+#
+stty columns 120
+
+# alias
+#
+alias vi=$(brew --prefix ex-vi)/bin/vi
+alias vim=$(brew --prefix vim)/bin/vim
+alias neovim=$(brew --prefix neovim)/bin/nvim
