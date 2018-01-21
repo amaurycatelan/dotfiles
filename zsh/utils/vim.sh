7da18e3cf39b5ac332ca7616d970d1bf199f959c
Amaury <amaury.catelan@gmail.com>
2018-01-21 19:42:18 -0200

    Update vim alias with stty columns

--- zsh/utils/vim.sh
+++ zsh/utils/vim.sh
@@ -3,12 +3,13 @@
 # VIM ,,
 # --------------------------
 
-# fix vi error: Terminal too wide
-#
-stty columns 120
+function vi() {
+  itermcol=$(stty size | cut -d ' ' -f 2) &&
+  stty columns 120 &&
+  $(brew --prefix ex-vi)/bin/vi &&
+  stty columns ${itermcol}
+}
 
-# alias
-#
-alias vi=$(brew --prefix ex-vi)/bin/vi
+alias vi=vi
 alias vim=$(brew --prefix vim)/bin/vim
 alias neovim=$(brew --prefix neovim)/bin/nvim
