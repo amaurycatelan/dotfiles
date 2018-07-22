8b5be11644d4215a0453092fc524151ea2985d07
Amaury <amaury.catelan@gmail.com>
2018-07-22 19:38:28 -0300

    Remove verify docker-machine status

--- zsh/utils/docker.sh
+++ zsh/utils/docker.sh
@@ -8,8 +8,3 @@ DOCKER_MACHINE="default"
 alias docker-init="docker-machine start ${DOCKER_MACHINE} && sleep 3 && docker-setup"
 alias docker-stop="docker-machine stop ${DOCKER_MACHINE} && sleep 1"
 alias docker-setup='eval "$(docker-machine env ${DOCKER_MACHINE})"'
-
-if docker-machine status $DOCKER_MACHINE | grep "Running" &> /dev/null
-  then
-    eval "$(docker-machine env $DOCKER_MACHINE)"
-fi
