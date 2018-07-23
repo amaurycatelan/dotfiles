bef09aeed1923def2ed88d230e4faf1f831f8526
Amaury <amaury.catelan@gmail.com>
2018-07-22 21:39:31 -0300

    Add goenv function

--- bash/utils/lazyload.sh
+++ bash/utils/lazyload.sh
@@ -2,3 +2,15 @@
 
 # LAZYLOAD ,,
 # --------------------------
+
+# GOENV (Go) ,,
+# --------------------------
+
+function load-goenv() {
+  export GOENV_ROOT="$HOME/.goenv"
+  export PATH="$GOENV_ROOT/bin:$PATH"
+  export GOPATH="$HOME/go"
+  eval "$(goenv init -)"
+}
+
+alias load-goenv=load-goenv
