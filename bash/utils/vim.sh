56f7f24207c930abce0e8bfc785383382f90d1e6
Amaury <amaury.catelan@gmail.com>
2018-07-22 21:37:38 -0300

    Remove brew --prefix

--- bash/utils/vim.sh
+++ bash/utils/vim.sh
@@ -6,10 +6,10 @@
 function vi() {
   itermcol=$(stty size | cut -d ' ' -f 2) &&
   stty columns 120 &&
-  $(brew --prefix ex-vi)/bin/vi &&
+  /usr/local/opt/ex-vi/bin/vi &&
   stty columns ${itermcol}
 }
 
 alias vi=vi
-alias vim=$(brew --prefix vim)/bin/vim
-alias neovim=$(brew --prefix neovim)/bin/nvim
+alias vim=/usr/local/opt/vim/bin/vim
+alias neovim=/usr/local/opt/neovim/bin/nvim
