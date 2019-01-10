22ea33312464f1285fce67e3d4f4e7be6f816898
Amaury <3202177+amaurycatelan@users.noreply.github.com>
2019-01-10 02:19:04 -0200

    update sublime build with automatic read files on this directory

--- sublime/.build.sh
+++ sublime/.build.sh
@@ -9,13 +9,7 @@ __sublime() {
   local build=3176
   local application="/Applications/Sublime Text.app"
   local settings="${HOME}/Library/Application Support/Sublime Text 3/Packages/User"
-  local dotfiles="${HOME}/dotfiles/sublime/"
-
-  local files=(
-    "Preferences.sublime-settings"
-    "Package Control.sublime-settings"
-    "Default (OSX).sublime-keymap"
-  )
+  local dotfiles="${HOME}/dotfiles/sublime"
 
   # parameters
   # --------------------------
@@ -29,10 +23,13 @@ __sublime() {
 
   local i=0
 
-  for item in "${files[@]}"; do
-    local command=("${dotfiles}${item}" "${settings}");
-    echo "running #${i}: $command";
+  find ${dotfiles} ! '(' -name '*.DS_Store' -o -name '*.sh' ')' -type f -print0 |
+  while IFS= read -r -d $'\0' item; do
+
+    local command=("${item}" "${settings}");
+    echo "running #${i}: ${command[@]}"; i=$((i+1))
     cp -r "${command[@]}"; i=$((i+1))
+
   done
 
 }
